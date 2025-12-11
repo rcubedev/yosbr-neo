d1737a075db28263ae4b1f2ebcd5eb5e165aa82a
vercte <lopstedener@gmail.com>
2025-12-10 21:34:14 -0800

    you call THAT a neoforge port!?

--- src/main/java/me/shedaniel/yosbr/YourOptionsShallBeRespected.java
+++ src/main/java/me/shedaniel/yosbr/YourOptionsShallBeRespected.java
@@ -1,9 +1,7 @@
 package me.shedaniel.yosbr;
 
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.LanguageAdapter;
-import net.fabricmc.loader.api.LanguageAdapterException;
-import net.fabricmc.loader.api.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,22 +10,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-
-public class YourOptionsShallBeRespected implements LanguageAdapter {
+@Mod(YourOptionsShallBeRespected.ID)
+public class YourOptionsShallBeRespected {
+    public static final String ID = "yosbr";
     public static final Logger LOGGER = LogManager.getLogger("YOSBR");
-    public static final File RUN_DIR = FabricLoader.getInstance().getGameDirectory();
-    public static final File CONFIG_DIR = FabricLoader.getInstance().getConfigDirectory();
-    
-    /**
-     * We are using pre-launch entrypoint here as we want to be faster than everyone.
-     */
-    public YourOptionsShallBeRespected() {
+    public static final File RUN_DIR = FMLPaths.GAMEDIR.get().toFile();
+    public static final File CONFIG_DIR = FMLPaths.CONFIGDIR.get().toFile();
+
+    @SuppressWarnings("resource")
+    public static void respectTheOptions() {
         LOGGER.info("Applying default options... (YOSBR)");
         try {
             File yosbr = new File(CONFIG_DIR, "yosbr");
             if (!yosbr.exists() && !yosbr.mkdirs()) {
                 throw new IllegalStateException("Could not create directory: " + yosbr.getAbsolutePath());
             }
+
+            //noinspection ResultOfMethodCallIgnored
             new File(yosbr, "options.txt").createNewFile();
             File config = new File(yosbr, "config");
             if (!config.exists() && !config.mkdirs()) {
@@ -57,7 +56,7 @@ public class YourOptionsShallBeRespected implements LanguageAdapter {
         }
     }
     
-    private void applyDefaultOptions(File file, File defaultFile) throws IOException {
+    private static void applyDefaultOptions(File file, File defaultFile) throws IOException {
         if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
             throw new IllegalStateException("Could not create directory: " + file.getParentFile().getAbsolutePath());
         }
@@ -65,17 +64,14 @@ public class YourOptionsShallBeRespected implements LanguageAdapter {
             throw new IllegalStateException("Could not create directory: " + defaultFile.getParentFile().getAbsolutePath());
         }
         if (!defaultFile.exists()) {
+            //noinspection ResultOfMethodCallIgnored
             defaultFile.createNewFile();
             return;
         }
         if (file.exists()) return;
-        LOGGER.info("Applying default options for " + File.separator + RUN_DIR.toPath().toAbsolutePath().normalize().relativize(file.toPath().toAbsolutePath().normalize()).normalize().toString() + " from " + File.separator +
-                    RUN_DIR.toPath().toAbsolutePath().normalize().relativize(defaultFile.toPath().toAbsolutePath().normalize()).normalize().toString());
+        LOGGER.info("Applying default options for {}{} from {}{}",
+                File.separator, RUN_DIR.toPath().toAbsolutePath().normalize().relativize(file.toPath().toAbsolutePath().normalize()).normalize().toString(),
+                File.separator, RUN_DIR.toPath().toAbsolutePath().normalize().relativize(defaultFile.toPath().toAbsolutePath().normalize()).normalize().toString());
         Files.copy(defaultFile.toPath(), file.toPath());
-    }
-    
-    @Override
-    public <T> T create(ModContainer mod, String value, Class<T> type) throws LanguageAdapterException {
-        throw new IllegalStateException();
     }
 }
